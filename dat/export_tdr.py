96fb88ff0d43d53c40753706ddc49124f6028e80
Maxim Alferov <malferov44@gmail.com>
2019-02-10 00:15:17 +0100

    lambda select

--- dat/export_tdr.py
+++ dat/export_tdr.py
@@ -6,28 +6,31 @@ import pandas as pd
 import os
 from pdb import set_trace as bp
 
+size = 17
+
 with open('../outlogTDR.txt') as fs:
-    df = pd.read_csv(fs, header=None, names=range(17), skipinitialspace=True, engine='python')
+    df = pd.read_csv(fs, header=None, names=range(size), skipinitialspace=True, engine='python')
     fs.close
+    a = lambda col, pos : df[col].str.split(' ').str[pos]
     df['date']          = df[0].str[0:19]
-    df['tdrnum']        = df[0].str.split(' ').str[2]
-    df['bl']            = df[1].str.split(' ').str[1]
-    df['zamount']       = df[2].str.split(' ').str[1]
-    df['zamount_ready'] = df[3].str.split(' ').str[1]
-    df['period']        = df[4].str.split(' ').str[1]
-    df['rate']          = df[5].str.split(' ').str[1]
-    df['backamount']    = df[6].str.split(' ').str[1]
-    df['backcnt']       = df[7].str.split(' ').str[1]
-    df['debt']          = df[8].str.split(' ').str[1]
-    df['rating']        = df[9].str.split(' ').str[1]
-    df['r1']            = df[9].str.split(' ').str[2]
-    df['r2']            = df[9].str.split(' ').str[3]
-    df['r3']            = df[9].str.split(' ').str[4]
-    df['camount']       = df[10].str.split(' ').str[1]
-    df['creditamount']  = df[11].str.split(' ').str[1]
-    df['debtper']       = df[12].str.split(' ').str[1]
-    df['ramt']          = df[13].str.split(' ').str[1]
-    df['paid']          = df[14].str.split(' ').str[1]
+    df['tdrnum']        = a(0, 2)
+    df['bl']            = a(1, 1)
+    df['zamount']       = a(2, 1)
+    df['zamount_ready'] = a(3, 1)
+    df['period']        = a(4, 1)
+    df['rate']          = a(5, 1)
+    df['backamount']    = a(6, 1)
+    df['backcnt']       = a(7, 1)
+    df['debt']          = a(8, 1)
+    df['rating']        = a(9, 1)
+    df['r1']            = a(9, 2)
+    df['r2']            = a(9, 3)
+    df['r3']            = a(9, 4)
+    df['camount']       = a(10, 1)
+    df['creditamount']  = a(11, 1)
+    df['debtper']       = a(12, 1)
+    df['ramt']          = a(13, 1)
+    df['paid']          = a(14, 1)
 with open('rating.csv', 'w') as fp:
     fp.write(df.to_csv(index = False, columns = [
         'date', 'tdrnum', 'bl', 'zamount', 'zamount_ready', 'period', 'rate', 
