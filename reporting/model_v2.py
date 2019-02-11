dbfe065c3c8060f994ff47d200d7df388e5a1eb0
Maxim Alferov <malferov44@gmail.com>
2019-02-12 00:37:44 +0100

    bl and debt correlation

---
+++ reporting/model_v2.py
@@ -0,0 +1,12 @@
+"""
+rating.csv
+rating, r1, r2, r3 -> type INTEGER
+
+model version
+starting 2016-06-03 00:00 -> v1
+"""
+
+R3NORM = [200, 50]
+r3 = 0.5 (1 + erf(x))
+x = (bl - R3NORM[0]) * 2 / R3NORM[1]
+R3W = 0.9
--- reporting/rating.sql
+++ reporting/rating.sql
@@ -6,12 +6,12 @@ create TEMP table correlation as
 select 
 case when o.camt - o.ramt > 0 then (strftime('%s','now') - o.date)/86400 - r.period
 else (o.rdate - o.date)/86400 - r.period end delinq,
-r.rating, r.r1, r.r2, r.r3
+r.rating, r.r1, r.r2, r.r3, r.bl, r.debtper,
+round((r.creditamount - r.backamount) / r.creditamount * 100, 1) as debt
 from operation o
 inner join portfolio p on p.CDWMTranID = o.id
 inner join rating r on r.tdrnum = p.CTenderID
-where r.rating < 1000 and
-o.date > strftime('%s','2016-06-03') and -- the last rating model change
-strftime('%s','now') - o.date - r.period * 86400 > 0;
+where o.date > strftime('%s','2016-06-03') -- rating model v1
+and strftime('%s','now') - o.date - r.period * 86400 > 0; -- matured
 
 select * from correlation;
