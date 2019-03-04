3064c8d68f70747f00979a311b15965d6e6fe0be
Maxim Alferov <malferov44@gmail.com>
2019-03-04 20:17:29 +0100

    extend deals add rate

--- reporting/delinq.sql
+++ reporting/delinq.sql
@@ -1,33 +1,4 @@
 /*
-  denormalized deals source
-  TODO add rate to the deals source
-*/
-drop table if exists deals;
-create TEMP table deals as
-select cast(o.date as INT) as odate,
-datetime(o.date, 'unixepoch', 'localtime') as dealdate,
-cast(r.period as INT) as period,
-cast(o.camt as REAL) as camt,
-cast(o.ramt as REAL) as ramt,
-cast(o.camt-o.ramt as REAL) as outstanding,
-datetime(o.rdate, 'unixepoch', 'localtime') as returndate,
-cast(o.date + r.period * 86400 as INT) as mdate,
-datetime(o.date + r.period * 86400, 'unixepoch', 'localtime') as maturitydate, 
-cast(case when o.camt - o.ramt > 0 then (strftime('%s','now') - o.date)/86400 - r.period
-else (o.rdate - o.date)/86400 - r.period end as INT) as delinq,
-cast(r.rating as INT) as rating,
-cast(r.r1 as INT) as r1,
-cast(r.r2 as INT) as r2,
-cast(r.r3 as INT) as r3,
-cast(r.bl as INT) as bl,
-cast(round((r.creditamount - r.backamount) / r.creditamount * 100, 1) as REAL) as debt,
-cast(r.creditamount as REAL) as creditamount,
-case when o.date < strftime('%s','2016-06-03') then 'v0' else 'v1' end model
-from operation o
-inner join portfolio p on p.CDWMTranID = o.id
-inner join rating r on r.tdrnum = p.CTenderID;
-
-/*
   delinquency group by active, overdue, delinq_60, delinq_360, delinq_def
 */
 drop table if exists delinq;
--- reporting/rating.sql
+++ reporting/rating.sql
@@ -8,13 +8,21 @@ creditamount > 1000 and creditamount <= 2000 as cr2,
 creditamount > 2000 and creditamount <= 3000 as cr3,--32815
 creditamount > 3000 and creditamount <= 4000 as cr4,
 creditamount > 4000 and creditamount <= 5000 as cr5,
-debt = 0 as debt0,
+creditamount > 5000 and creditamount <= 6000 as cr6,
+creditamount > 6000 and creditamount <= 7000 as cr7,
+creditamount > 7000 and creditamount <= 8000 as cr8,
+creditamount > 8000 and creditamount <= 9000 as cr9,
+creditamount > 9000 and creditamount <= 10000 as cr10,
+creditamount > 10000 and creditamount <= 20000 as cr20,
+creditamount > 20000 and creditamount <= 50000 as cr50,
+creditamount > 50000 and creditamount <= 100000 as cr100,
+--debt = 0 as debt0,
 count(camt), sum(camt), sum(outstanding)
---delinq, rating, r1, r2, r3, bl, debt, outstanding, creditamount
+--select delinq, rating, r1, r2, r3, bl, debt, outstanding, creditamount
 from deals
 where model = 'v1'
 and strftime('%s','now') - mdate > 0 -- matured
 and outstanding > 0
 and delinq > 90
 --order by cast(creditamount as decimal)
-group by cr0, cr1, cr2, cr3, cr4, cr5, debt0
+group by cr0, cr1, cr2, cr3, cr4, cr5, cr6, cr7, cr8, cr9, cr10, cr20, cr50, cr100--, debt0
